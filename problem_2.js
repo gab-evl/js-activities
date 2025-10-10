24198de8343479dd047837021c7685a26a989dbc
gab-evl <Gabriel.Velasquez@olivarezcollegetagaytay.edu.ph>
2025-10-10 07:54:23 +0000

    Yes

---
+++ problem_2.js
@@ -0,0 +1,5 @@
+let fareheit = 100;
+
+let celsius = (fareheit - 32) * 5 / 9;
+
+console.log("100F to Celsius: is: " + celsius);
---
+++ promblem_1.js
@@ -0,0 +1,11 @@
+let num_1 = 20;
+let num_2 = 5;
+
+let sum = num_1 + num_2;
+let difference = num_1 - num_2
+
+
+console.log("The first number is: " + num_1);
+console.log("The second number is: " + num_2);
+console.log("The sum of 20 and 5 is: " + sum);
+console.log("The difference sum of 20 and 5 is: " + difference);

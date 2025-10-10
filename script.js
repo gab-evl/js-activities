10090c0db63013e2c6346239fa26257e634ae30d
gab-evl <Gabriel.Velasquez@olivarezcollegetagaytay.edu.ph>
2025-10-10 15:21:31 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const PI = 3.14159;
+
+let radius = 5;
+
+let area = PI * radius * radius;
+
+console.log("The radius of the circle is: " + radius);
+console.log("The value of PI is: " + PI);
+console.log("The area of the circle is: " + area);
